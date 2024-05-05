47e7fd1502484474cfe3460307992c29ee9039d6
Memai2023 <maria.hong26@gmail.com>
2024-05-05 22:06:21 +0200

    restore origin main

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Artifacts from './Pages/Artifacts'
+import Essence from './Pages/Essence'
+import Home from './Pages/Home'
+import ScrumTeam from './Pages/ScrumTeam'
+import SprintEvents from './Pages/SprintEvents'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='essence' element={<Essence />}/>
+        <Route path='artifacts' element={<Artifacts />}/>
+        <Route path='scrum-team' element={<ScrumTeam />}/>
+        <Route path='events' element={<SprintEvents />}/>
+      </Routes>
     </>
   )
 }
