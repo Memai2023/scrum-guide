a56f5678850a759f07c2ea607f7681cf1b0a81fa
Memai2023 <maria.hong26@gmail.com>
2024-05-06 00:54:06 +0200

    restore essence-whatisscrum-scrum-24

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,13 @@
-import Artifacts from './Pages/Artifacts'
-import Essence from './Pages/Essence'
-import Home from './Pages/Home'
-import ScrumTeam from './Pages/ScrumTeam'
-import SprintEvents from './Pages/SprintEvents'
-import { Routes, Route } from 'react-router-dom'
+import { useState } from 'react'
+import WhatIsScrum from './Components/Essence/WhatIsScrum'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='essence' element={<Essence />}/>
-        <Route path='artifacts' element={<Artifacts />}/>
-        <Route path='scrum-team' element={<ScrumTeam />}/>
-        <Route path='events' element={<SprintEvents />}/>
-      </Routes>
+            <WhatIsScrum />
+
     </>
   )
 }
