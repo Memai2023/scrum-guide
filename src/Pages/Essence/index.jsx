ee917cc584d5ddbb61033910e23c1635b63a45d9
Africandeluxe <miski.mohamedahmed@gmail.com>
2024-05-07 18:49:23 +0200

    Added essence component o page and fixed css

--- src/Pages/Essence/index.jsx
+++ src/Pages/Essence/index.jsx
@@ -1,20 +1,10 @@
-<<<<<<< HEAD
 import React from 'react';
-import ThreePillars from '../../Components/Essence/ThreePillarsInfo';
-
-const Essence = () => {
-    return(
-        <>
-        <ThreePillars />
-        </>
-    );
-};
-=======
 import WhatIsScrum from "../../Components/Essence/WhatIsScrum"
 import History from "../../Components/Essence/History"
 import HouseOfScrum from "../../Components/Essence/HouseOfScrum"
 import Theory from "../../Components/Essence/Theory"
 import ThreePillars from "../../Components/Essence/ThreePillars"
+import ThreePillarsInfo from '../../Components/Essence/ThreePillarsInfo';
 import styles from './Essence.module.css'
 
 const Essence = () => {
@@ -25,9 +15,9 @@ const Essence = () => {
             <Theory />
             <HouseOfScrum />
             <ThreePillars />
+            <ThreePillarsInfo />
         </div>
     )
 }
->>>>>>> main
 
 export default Essence;
