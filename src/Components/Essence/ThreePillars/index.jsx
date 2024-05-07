f82c100b7347886de0500a257d51a04664c11800
Africandeluxe <miski.mohamedahmed@gmail.com>
2024-05-07 12:04:49 +0200

    CR feedback done

--- src/Components/Essence/ThreePillars/index.jsx
+++ src/Components/Essence/ThreePillars/index.jsx
@@ -4,18 +4,18 @@ import styles from './ThreePillars.module.css';
 
 const ThreePillars = () => {
   return (
-    <div className={styles.card}>
-      <div className={styles.leftColumn}>
-        <div className={styles.header}>
+  <div className={styles.card}>
+    <div className={styles.leftColumn}>
+      <div className={styles.header}>
         The three pillars of empiricism
       </div>
-      </div>
-      <div className={styles.rightColumn}>
-        <div className={styles.body}>
+    </div>
+    <div className={styles.rightColumn}>
+       <div className={styles.body}>
         Framework of development needs pillars to be supported in the concept of working iteratively and to make it resilient to adverse effects. For Scrum these pillars are:
       </div>
-      </div>
-      </div>
+    </div>
+  </div>
   );
 };
 
