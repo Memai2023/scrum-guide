0d2174465dad0fc57be7f8e6e3e952b312247cdc
Africandeluxe <miski.mohamedahmed@gmail.com>
2024-05-07 13:16:30 +0200

    Sprint events two - scrum35

--- src/Pages/SprintEvents/index.jsx
+++ src/Pages/SprintEvents/index.jsx
@@ -1,9 +1,12 @@
+import React from 'react';
+import SprintEventsInfoTwo from "../../Components/SprintEvents/SprintEventsInfoTwo";
+
 const SprintEvents = () => {
     return(
-        <>
-        
-        </>
+        <div>
+            <SprintEventsInfoTwo />
+        </div>
     )
 }
 
-export default SprintEvents
+export default SprintEvents;
