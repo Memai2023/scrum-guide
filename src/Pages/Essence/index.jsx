a19c850b54cbd5893a0321911cf694b717b02e17
Lisette Svan <svanlisette@gmail.com>
2024-05-07 23:03:08 +0200

    imported heroimage in pages

--- src/Pages/Essence/index.jsx
+++ src/Pages/Essence/index.jsx
@@ -5,11 +5,13 @@ import HouseOfScrum from "../../Components/Essence/HouseOfScrum"
 import Theory from "../../Components/Essence/Theory"
 import ThreePillars from "../../Components/Essence/ThreePillars"
 import ThreePillarsInfo from '../../Components/Essence/ThreePillarsInfo';
+import HeroImage from '../../Components/Essence/HeroImage'
 import styles from './Essence.module.css'
 
 const Essence = () => {
     return(
         <div className={styles.essenceContainer}>
+            <HeroImage />
             <WhatIsScrum/>
             <History />
             <Theory />
