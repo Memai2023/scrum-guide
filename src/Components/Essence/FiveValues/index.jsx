9cf0865e4a3e20e8e6ae8f27086df1b5ab224049
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-05-08 09:25:24 +0200

    fix indentation

--- src/Components/Essence/FiveValues/index.jsx
+++ src/Components/Essence/FiveValues/index.jsx
@@ -7,31 +7,32 @@ const FiveValues = () => {
             <div className={styles.container}>
                 <h1 className={styles.title}>The 5 values of Scrum</h1>
                 <p className={styles.sideText}>{Essence.fiveValuesText}</p>
+            </div>
+            <div className={styles.mainContainer}>
+                <div className={styles.circle}>
+                    <h2 className={styles.circleTitle}>Commitment</h2>
+                    <p>{Essence.commitmentCircleText}</p>
                 </div>
-                <div className={styles.mainContainer}>
-                    <div className={styles.circle}>
-                        <h2 className={styles.circleTitle}>Commitment</h2>
-                        <p>{Essence.commitmentCircleText}</p>
-                    </div>
-                    <div className={styles.circle}>
-                        <h2 className={styles.circleTitle}>Courage</h2>
-                        <p>{Essence.courageCircleText}</p>
-                    </div>
-                    <div className={styles.circle}>
-                        <h2 className={styles.circleTitle}>Focus</h2>
-                        <p>{Essence.focusCirleText}</p>
-                    </div>
-                    <div className={styles.circle}>
-                        <h2 className={styles.circleTitle}>Openness</h2>
-                        <p>{Essence.openessCircleText}</p>
-                    </div>
-                    <div className={styles.circle}>
-                        <h2 className={styles.circleTitle}>Respect</h2>
-                        <p>{Essence.respectCircleText}</p>
-                    </div>
+                <div className={styles.circle}>
+                    <h2 className={styles.circleTitle}>Courage</h2>
+                    <p>{Essence.courageCircleText}</p>
                 </div>
+                <div className={styles.circle}>
+                    <h2 className={styles.circleTitle}>Focus</h2>
+                    <p>{Essence.focusCirleText}</p>
+                </div>
+                <div className={styles.circle}>
+                    <h2 className={styles.circleTitle}>Openness</h2>
+                    <p>{Essence.openessCircleText}</p>
+                </div>
+                <div className={styles.circle}>
+                    <h2 className={styles.circleTitle}>Respect</h2>
+                    <p>{Essence.respectCircleText}</p>
+                </div>
+            </div>
             <hr className={styles.lines} />
         </div>
     );
 };
+
 export default FiveValues;
