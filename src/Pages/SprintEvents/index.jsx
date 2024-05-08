ad8d3d164402e4fc5540a4607a96abcc0cd198e0
Wilma NOrman <wilmanorman999@gmail.com>
2024-05-08 12:50:47 +0200

    merged to develop

--- src/Pages/SprintEvents/index.jsx
+++ src/Pages/SprintEvents/index.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import SprintEventsInfoOne from "../../Components/SprintEvents/SpringEventsInfoOne";
 import SprintEventsInfoTwo from "../../Components/SprintEvents/SprintEventsInfoTwo";
-
-
 import EventsHero from "../../Components/SprintEvents/Hero"
 
 const SprintEvents = () => {
     return(
         <div>
+            <EventsHero />
             <SprintEventsInfoOne/>
-            <SprintEventsInfoTwo /> <EventsHero />
+            <SprintEventsInfoTwo /> 
         </div>
     )
 }
