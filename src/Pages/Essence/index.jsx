3f32eb87b4c5f51665d006c1f660732c1fa8a0e0
Lisette Svan <svanlisette@gmail.com>
2024-05-08 11:04:37 +0200

    fixed picture path

--- src/Pages/Essence/index.jsx
+++ src/Pages/Essence/index.jsx
@@ -2,11 +2,13 @@ import WhatIsScrum from "../../Components/Essence/WhatIsScrum"
 import History from "../../Components/Essence/History"
 import HouseOfScrum from "../../Components/Essence/HouseOfScrum"
 import Theory from "../../Components/Essence/Theory"
+import HeroImage from "../../Components/Essence/HeroImage"
 import styles from './Essence.module.css'
 
 const Essence = () => {
     return(
         <div className={styles.essenceContainer}>
+            <HeroImage />
             <WhatIsScrum/>
             <History />
             <Theory />
