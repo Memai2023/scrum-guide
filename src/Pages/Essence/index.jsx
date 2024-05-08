16c1575db1fbdd945e3fb98164cae785184fee35
Africandeluxe <miski.mohamedahmed@gmail.com>
2024-05-08 21:10:35 +0200

    Scrum22 merged to staging. Added component to essence page

--- src/Pages/Essence/index.jsx
+++ src/Pages/Essence/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import ThreePillars from '../../Components/Essence/ThreePillarsInfo';
-import WhatIsScrum from "../../Components/Essence/WhatIsScrum"
-import History from "../../Components/Essence/History"
-import HouseOfScrum from "../../Components/Essence/HouseOfScrum"
-import Theory from "../../Components/Essence/Theory"
-import ThreePillars from "../../Components/Essence/ThreePillars"
-import styles from './Essence.module.css'
+import WhatIsScrum from "../../Components/Essence/WhatIsScrum";
+import History from "../../Components/Essence/History";
+import HouseOfScrum from "../../Components/Essence/HouseOfScrum";
+import Theory from "../../Components/Essence/Theory";
+import ThreePillars from "../../Components/Essence/ThreePillars";
+import ThreePillarsInfo from "../../Components/Essence/ThreePillarsInfo";
+import styles from './Essence.module.css';
 
 const Essence = () => {
     return(
@@ -15,6 +15,7 @@ const Essence = () => {
             <Theory />
             <HouseOfScrum />
             <ThreePillars />
+            <ThreePillarsInfo />
         </div>
     )
 }
