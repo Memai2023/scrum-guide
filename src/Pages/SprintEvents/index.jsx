6adddc057f91eafc637932f31cab765207901624
Africandeluxe <miski.mohamedahmed@gmail.com>
2024-05-09 14:51:55 +0200

    Fixed bugg in sprint event page

--- src/Pages/SprintEvents/index.jsx
+++ src/Pages/SprintEvents/index.jsx
@@ -1,22 +1,15 @@
 import React from 'react';
 import SprintEventsInfoOne from "../../Components/SprintEvents/SpringEventsInfoOne";
 import SprintEventsInfoTwo from "../../Components/SprintEvents/SprintEventsInfoTwo";
-<<<<<<< HEAD
 import EventsHero from "../../Components/SprintEvents/Hero"
-=======
->>>>>>> 4a5948c211bdc7d2a95a0e74b9e9a0570f583cb5
 import SprintCycle2 from "../../Components/SprintEvents/SprintCycle2"
 
 const SprintEvents = () => {
     return(
         <div>
             <EventsHero />
-            <SprintEventsInfoOne/>
-<<<<<<< HEAD
-            <SprintEventsInfoTwo />     
-=======
-            <SprintEventsInfoTwo />
->>>>>>> 4a5948c211bdc7d2a95a0e74b9e9a0570f583cb5
+            <SprintEventsInfoOne />
+            <SprintEventsInfoTwo />   
             <SprintCycle2 />
         </div>
     )
