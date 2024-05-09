ed29b3f7506ec7e4a8e8124364c349713ea06106
Memai2023 <maria.hong26@gmail.com>
2024-05-09 19:16:33 +0200

    fix the scroll on top of the page

--- src/Components/Buttons/index.jsx
+++ src/Components/Buttons/index.jsx
@@ -1,22 +1,21 @@
 import { ChevronLeft, ChevronRight } from "lucide-react";
-import { Link, useHistory } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import styles from './Buttons.module.css'
 
 const Buttons = ({direction, path}) => {  
-    const history = useHistory();
 
     const handleClick = () => {
-        window.scrollTo(0, 0);
-        history.push(path);
-    }
+    window.scrollTo(0, 0);
+    };
+
     return (
         <>
             {direction === "forward" ? (
-                <Link to={path} className={styles.arrow}>
+                <Link to={path} className={styles.arrow} onClick={handleClick}>
                     <ChevronRight/>
                 </Link>
             ):(
-                <Link to={path} className={styles.arrow}>
+                <Link to={path} className={styles.arrow} onClick={handleClick}>
                     <ChevronLeft/>
                 </Link>
             )}
