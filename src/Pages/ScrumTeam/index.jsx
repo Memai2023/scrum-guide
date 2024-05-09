126e8719ced761234b63aab7862305ba5e84b8a5
Africandeluxe <miski.mohamedahmed@gmail.com>
2024-05-09 14:57:12 +0200

    Fixed bugg on scrum team page

--- src/Pages/ScrumTeam/index.jsx
+++ src/Pages/ScrumTeam/index.jsx
@@ -1,36 +1,16 @@
-<<<<<<< HEAD
 import React from 'react';
 import ScrumTeamHero from "../../Components/ScrumTeam/Hero";
-=======
-<<<<<<< HEAD
 import TreeCirkles from "../../Components/ScrumTeam/ThreeCirkles"
-
-const ScrumTeam = () => {
-    return(
-        <>
-            <TreeCirkles />
-        </>
-=======
->>>>>>> three-cirkles-scrum32
 import ProductOwner from "../../Components/ScrumTeam/ProductOwner"
 
 const ScrumTeam = () => {
-<<<<<<< HEAD
     return (
     <div>
         <ScrumTeamHero />
         <ProductOwner />
+        <TreeCirkles />
     </div>
     );
 };
-=======
-    return(
-        <div className={styles.scrumTeamContainer}>
-            <ProductOwner />
-        </div>
->>>>>>> main
-    )
-}
->>>>>>> three-cirkles-scrum32
 
 export default ScrumTeam;
