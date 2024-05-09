9094a6269b6f0842c058c0d744adfa6b107dbf15
Memai2023 <maria.hong26@gmail.com>
2024-05-09 19:02:34 +0200

    code for scrolling up in the next page after buttonclick

--- src/Components/Buttons/index.jsx
+++ src/Components/Buttons/index.jsx
@@ -1,8 +1,14 @@
 import { ChevronLeft, ChevronRight } from "lucide-react";
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import styles from './Buttons.module.css'
 
 const Buttons = ({direction, path}) => {  
+    const history = useHistory();
+
+    const handleClick = () => {
+        window.scrollTo(0, 0);
+        history.push(path);
+    }
     return (
         <>
             {direction === "forward" ? (
