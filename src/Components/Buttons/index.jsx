5982444769254d4002be31cf9b054a35ac5430f1
Memai2023 <maria.hong26@gmail.com>
2024-05-09 19:31:02 +0200

    cleaned up the code

--- src/Components/Buttons/index.jsx
+++ src/Components/Buttons/index.jsx
@@ -3,23 +3,23 @@ import { Link } from 'react-router-dom'
 import styles from './Buttons.module.css'
 
 const Buttons = ({direction, path}) => {  
-
     const handleClick = () => {
-    window.scrollTo(0, 0);
+    window.scrollTo(0, 0); 
     };
 
     return (
         <>
             {direction === "forward" ? (
-                <Link to={path} className={styles.arrow} onClick={handleClick}>
-                    <ChevronRight/>
-                </Link>
-            ):(
-                <Link to={path} className={styles.arrow} onClick={handleClick}>
-                    <ChevronLeft/>
-                </Link>
+            <Link to={path} className={styles.arrow} onClick={handleClick}>
+                <ChevronRight/>
+            </Link>
+        ):(
+            <Link to={path} className={styles.arrow} onClick={handleClick}>
+                <ChevronLeft/>
+            </Link>
             )}
         </>
     )
 }
+
 export default Buttons;
