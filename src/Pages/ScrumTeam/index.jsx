f825560163ba6b0ac5abf3dff51b69b30923c669
Wilma NOrman <wilmanorman999@gmail.com>
2024-05-10 14:29:33 +0200

    looking at files no changes

--- src/Pages/ScrumTeam/index.jsx
+++ src/Pages/ScrumTeam/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ScrumTeamHero from "../../Components/ScrumTeam/Hero";
 import ProductOwner from "../../Components/ScrumTeam/ProductOwner"
 import TreeCirkles from "../../Components/ScrumTeam/ThreeCirkles"
+import styles from './ScrumTeam.module.css'
 
 const ScrumTeam = () => {
     return(
