1df6ebaf647daa1db8f3861933fc916c1d8c2e88
Africandeluxe <miski.mohamedahmed@gmail.com>
2024-05-10 16:28:32 +0200

    changes to scrum team page

--- src/Pages/ScrumTeam/index.jsx
+++ src/Pages/ScrumTeam/index.jsx
@@ -1,27 +1,16 @@
-<<<<<<< HEAD
 import React from 'react';
 import ScrumTeamHero from "../../Components/ScrumTeam/Hero";
-import ScrumTeamInfo from '../../Components/ScrumTeam/ScrumTeamInfo';
+import ProductOwner from "../../Components/ScrumTeam/ProductOwner"
 
 const ScrumTeam = () => {
     return (
     <div>
         <ScrumTeamHero />
         <ScrumTeamInfo />
-        </div>
+        <ProductOwner />
+    </div>
     );
 };
-=======
-import ProductOwner from "../../Components/ScrumTeam/ProductOwner"
-import styles from './ScrumTeam.module.css'
 
-const ScrumTeam = () => {
-    return(
-        <div className={styles.scrumTeamContainer}>
-            <ProductOwner />
-        </div>
-    )
-}
->>>>>>> main
 
 export default ScrumTeam;
