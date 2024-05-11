61e3b75eef123981199f3cdf8df57f7aa4dd0c61
Memai2023 <maria.hong26@gmail.com>
2024-05-11 17:57:06 +0200

    merging solving in pages

--- src/Pages/ScrumTeam/index.jsx
+++ src/Pages/ScrumTeam/index.jsx
@@ -1,15 +1,13 @@
-<<<<<<<<< Temporary merge branch 1
 import React from 'react';
 import ScrumTeamHero from "../../Components/ScrumTeam/Hero";
 import TreeCirkles from "../../Components/ScrumTeam/ThreeCirkles"
-import ScrumHero from "../../Components/ScrumTeam/Hero"
 import ProductOwner from "../../Components/ScrumTeam/ProductOwner"
 import DevelopTeam from '../../Components/ScrumTeam/DevelopTeam'
 
 const ScrumTeam = () => {
     return (
     <div>
-        <ScrumHero />
+        <ScrumTeamHero />
         <ProductOwner /> 
         <DevelopTeam />
         <TreeCirkles />
