beafe183a6d1670a8a2d551f6f8705e6a35dad86
Memai2023 <maria.hong26@gmail.com>
2024-05-13 00:33:49 +0200

    added backgroundcolor in sprints

--- src/Pages/SprintEvents/index.jsx
+++ src/Pages/SprintEvents/index.jsx
@@ -4,10 +4,11 @@ import Intro from "../../Components/SprintEvents/Intro"
 import SprintEventsInfoOne from "../../Components/SprintEvents/SpringEventsInfoOne";
 import SprintEventsInfoTwo from "../../Components/SprintEvents/SprintEventsInfoTwo";
 import SprintCycle2 from "../../Components/SprintEvents/SprintCycle2"
+import styles from "./Sprints.module.css"
 
 const SprintEvents = () => {
     return(
-        <div>
+        <div className={styles.sprintContainer}>
             <EventsHero />
             <Intro />
             <SprintEventsInfoOne/>
